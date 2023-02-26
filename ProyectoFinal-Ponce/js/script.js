21600591ace8c6d050a05ad38c33c58f6e0e2d9c
Cecilia Ponce <ceciliaponce28@gmail.com>
2023-02-26 17:40:01 -0300

    arreglo de total

--- ProyectoFinal-Ponce/js/script.js
+++ ProyectoFinal-Ponce/js/script.js
@@ -1,7 +1,7 @@
 const celulares = [
     {
         "nombre": "TCL 305",
-        "precio": 29.333,
+        "precio": 29333,
         "stock": 12,
         "img": "https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/t/c/tcl-305-frente_1.png",
         "id": 1,
@@ -9,7 +9,7 @@ const celulares = [
     },
     {
         "nombre": "TCL 305i",
-        "precio": 35.656,
+        "precio": 35656,
         "stock": 5,
         "img": "https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/i/m/image_7__1.png",
         "id": 2,
@@ -17,7 +17,7 @@ const celulares = [
     },
     {
         "nombre": "SAMSUNG GALAXY A13 128GB",
-        "precio": 32.434,
+        "precio": 32434,
         "stock": 3,
         "img": "https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/a/1/a13-negro-frente_1.png",
         "id": 3,
@@ -25,7 +25,7 @@ const celulares = [
     },
     {
         "nombre": "SAMSUNG GALAXY S22 plus 5G",
-        "precio": 36.888,
+        "precio": 36888,
         "stock": 5,
         "img": "https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/s/2/s22_-blanco-frente.png",
         "id": 4,
@@ -33,7 +33,7 @@ const celulares = [
     },
     {
         "nombre": "SAMSUNG GALAXY A33 5G",
-        "precio": 43.299,
+        "precio": 43299,
         "stock": 7,
         "img": "https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/a/3/a33-negro-frente_1.png",
         "id": 5,
@@ -41,7 +41,7 @@ const celulares = [
     },
     {
         "nombre": "SAMSUNG GALAXY A04S 128GB",
-        "precio": 34.656,
+        "precio": 34656,
         "stock": 11,
         "img": "https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/f/r/frente_15.png",
         "id": 6,
@@ -49,7 +49,7 @@ const celulares = [
     },
     {
         "nombre": "SAMSUNG GALAXY S20 FE 5G",
-        "precio": 25.776,
+        "precio": 25776,
         "stock": 2,
         "img": "https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/s/2/s20-azul-frente_1.png",
         "id": 7,
@@ -58,7 +58,7 @@ const celulares = [
     
     {
         "nombre": "MOTOROLA EDGE 30 5G",
-        "precio": 29.999,
+        "precio": 29999,
         "stock": 1,
         "img": "https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/e/d/edge30-plata-frente.png",
         "id": 8,
@@ -66,7 +66,7 @@ const celulares = [
     },
     {
         "nombre": "MOTOROLA MOTO G22",
-        "precio": 40.599,
+        "precio": 40599,
         "stock": 4,
         "img": "https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/g/2/g22-negro-frente.png",
         "id": 9,
@@ -74,7 +74,7 @@ const celulares = [
     },
     {
         "nombre": "MOTOROLA MOTO E32",
-        "precio": 45.374,
+        "precio": 45374,
         "stock": 4,
         "img": "https://tienda.movistar.com.ar/media/catalog/product/cache/1d01ed3f1ecf95fcf479279f9ae509ad/m/o/motoe32-frente.png",
         "id": 10,
@@ -107,8 +107,7 @@ celulares.forEach((producto) => {
 })
 
 
-let totalProd = 0;
-let total =0;
+
 let envio = 1000;
 let precioApagar ;
 const carrito = [];
@@ -184,8 +183,7 @@ const {id, nombre, stock, precio, img, aVender} = producto
 
 
 
-// contadorCarro.textContent = carrito.length
-
+total.innerText = "Total: $ " + carrito.reduce((acc,producto) => acc + Number(producto.precio * producto.aVender) ,0)
 }
 
 
@@ -220,12 +218,8 @@ vaciarCarrito.addEventListener ('click', () => {
 
 
 // Total 
-//Problemas con unir la funcion con la cantidad
 
-
-const valorTotal = document.querySelector('.total')
-const cantProductos = document.querySelector('#contadorCarro')
-
+const total = document.getElementById("total")
 
 
 //alerta 
